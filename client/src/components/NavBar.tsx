7bcdce71d037bab651502d265adae862a0083aba
VSathveek <sathveekvaranasi@gmail.com>
2026-06-30 17:44:07 +0530

    Update navbar brand title

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
         <div className="w-[100%] h-[4rem] bg-gray-900 flex justify-between items-center flex-row p-[1rem]">
             <div onClick={() => navigate('/')} className="flex cursor-pointer flex-row gap-[0.5rem] justify-center items-center">
                 <img src={icon} className="h-[3rem] w-[3rem] rounded-full overflow-hidden" />
-                <p className="font-bold text-[1.1rem] font-mono text-[#fca311]">PodChamber</p>
+                <p className="font-bold text-[1.1rem] font-mono text-[#fca311]">TalkCircle</p>
             </div>
             <div>
                 {
